a935bd889704a3a3ee6280957a21eec483401cf1
Melanie Bott <melaniebott@Melanies-iMac.hsd1.ut.comcast.net>
2020-07-11 18:26:11 -0600

    Fixed bugs, now working and images added to app, sizing needs to be tweeked.

---
+++ src/components/Scoreboard/Scoreboard.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Scoreboard.css'
+
+//stateless component
+const Scoreboard = props => (
+  <div className='gameScore'>
+    <h3 className='score'>Your Score {props.total}</h3>
+    <h3 className='status'>{props.status}</h3>
+  </div>
+)
+
+export default Scoreboard
