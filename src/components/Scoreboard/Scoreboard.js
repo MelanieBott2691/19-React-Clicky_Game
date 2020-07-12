863d97f50a2bde85e10ed83ffbdbfcf4252ee47d
Melanie Bott <melaniebott@Melanies-iMac.hsd1.ut.comcast.net>
2020-07-11 20:30:25 -0600

    imaged added to jumbotron

--- src/components/Scoreboard/Scoreboard.js
+++ src/components/Scoreboard/Scoreboard.js
@@ -1,9 +1,8 @@
 import React from 'react'
 import './Scoreboard.css'
 
-//stateless component
 const Scoreboard = props => (
-  <div className='gameScore' align='right'>
+  <div className='gameScore'>
     <h3 className='score'>Your Score: {props.total}</h3>
     <h3 className='status'>{props.status}</h3>
   </div>
